9a16dd46126d88fdb400c13dbd52a206ed670e1c
Chokealot <casper.konyi@iths.se>
2021-11-07 11:35:35 +0100

    added a Main class to execute the program

---
+++ src/main/java/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+
+        Game game = new Game(10,10);
+        game.initializeGrid();
+        game.createRandomGame();
+        while (true) {
+            game.printGame();
+            game.findLife();
+            game.death();
+            game.birth();
+            Thread.sleep(1000);
+            System.out.println();
+        }
+    }
+}
