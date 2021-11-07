078575bc503ae8ac06ef16f6bcdde6ed66c65a21
Chokealot <casper.konyi@iths.se>
2021-11-07 10:38:29 +0100

    test for setSpecificNode()

--- src/test/java/GameTest.java
+++ src/test/java/GameTest.java
@@ -13,17 +13,23 @@ public class GameTest {
     }
 
     @Test
+    public void initializeGridWithSpecificWidthAndHeight() {
+        game.setGridHeight(7);
+        game.setGridWidth(7);
+        game.initializeGrid();
+        assertEquals(new int[7][7], game.grid);
+    }
+
+    @Test
     public void checkIfGridIsCreated() {
         int [][] expectedGrid = new int[3][3];
         assertEquals(expectedGrid, game.createEmptyGrid());
     }
 
     @Test
-    public void initializeGridWithSpecificWidthAndHeight() {
-        game.setGridHeight(7);
-        game.setGridWidth(7);
-        game.initializeGrid();
-        assertEquals(new int[7][7], game.grid);
+    public void setSpecificNodeToAliveFromDead() {
+        game.setSpecificNode(1,1);
+        assertEquals(1, game.grid[1][1]);
     }
 
 }
