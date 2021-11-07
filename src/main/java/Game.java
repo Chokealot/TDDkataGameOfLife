d79fe524e9af3ff5791183bb9bbe148ef279452e
Chokealot <casper.konyi@iths.se>
2021-11-07 10:48:55 +0100

    added findLife() in Game

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -33,7 +33,7 @@ public class Game {
     }
 
     public int[][] createEmptyGrid() {
-        for (int height=0; height < gridHeight; height++) {
+        for (int height = 0; height < gridHeight; height++) {
             for (int width = 0; width < gridWidth; width++) {
                 grid[height][width] = 0;
             }
@@ -44,5 +44,24 @@ public class Game {
     public void setSpecificNode(int height, int width) {
         if (grid[height][width] == 0) grid[height][width] = 1;
         else grid[height][width] = 0;
+    }
+
+    public void findLife() {
+        for (int y = 0; y < gridHeight; y++) {
+            for (int x = 0; x < gridWidth; x++) {
+                int count = 0;
+                for (int index1 = -1; index1 < 2; index1++) {
+                    if (y + index1 >= 0 && y + index1 < gridHeight) {
+                        for (int index2 = -1; index2 < 2; index2++) {
+                            if (x + index2 >= 0 && x + index2 < gridWidth) {
+                                if (!(y+index1 == y && index2+x == x) && grid[y + index1][x + index2] == 1)
+                                    count++;
+                            }
+                        }
+                    }
+                }
+                neighbours[y][x] = count;
+            }
+        }
     }
 }
